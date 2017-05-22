5b420d655e0e9060388c46ff25a4027b4fe22209
Kurt <kurtrm@gmail.com>
2017-05-22 14:03:21 -0700

    Added setup and tox files.

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""creating python distribution package setup.py"""
+from setuptools import setup
+
+setup(
+    name="http-server",
+    description="A script that uses a client and server to echo a message.",
+    version=0.1,
+    author="Morgan, Ronel, Kurt",
+    author_email="",
+    license='MIT',
+    py_modules=['client', 'server'],
+    package_dir={'': 'src'},
+    extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
+    entry_points={
+        'console_scripts': [
+            "client = client:main",
+            "server = server:main"
+        ]
+    }
+)
