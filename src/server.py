b9b5a3f2782f78e9278e652bb570eeddd40f8ac8
Morgan Nomura <morganelle@gmail.com>
2017-05-22 16:23:39 -0700

    all tests are passing except for a message that is a multiple of the buffer

--- src/server.py
+++ src/server.py
@@ -12,26 +12,31 @@ def server():
                            proto=socket.IPPROTO_TCP)
     address = ('127.0.0.1', 5000)
     server.bind(address)
-    server.listen(1)
-    conn, addr = server.accept()
 
-    client_message = ''
-    buffer_length = 8
-    complete = False
+    try:
+        while True:
+            server.listen(1)
+            conn, addr = server.accept()
+            client_message = ''
+            buffer_length = 8
+            complete = False
 
-    while not complete:
-        part = conn.recv(buffer_length)
-        client_message += part.decode('utf8')
-        if len(part) < buffer_length:
-            complete = True
+            while not complete:
+                part = conn.recv(buffer_length)
+                client_message += part.decode('utf8')
+                print('in while, part length:', len(part))
+                if len(part) < buffer_length:
+                    complete = True
 
-    print('server received: ', client_message)
-    client_message = client_message.encode('utf8')
-    conn.sendall(client_message)
-    conn.close()
+            print('server received: ', client_message)
+            client_message = client_message.encode('utf8')
+            conn.sendall(client_message)
+
+    except KeyboardInterrupt:
+        conn.close()
+        server.close()
+        sys.exit()
+
 
 if __name__ == '__main__':
-    try:
-        server()
-    except KeyboardInterrupt:
-        sys.exit()
+    server()
--- src/test_servers.py
+++ src/test_servers.py
@@ -5,17 +5,21 @@ import pytest
 
 
 PARAMS_TABLE = [
-    ("Hey Morgan"),
+    ("I Morgan"),
     ("abcdefghijklmnopqrstuveivmdnwjfidlwkfudjekweogutyfnfbvbcmdkedif"),
-    ("Hi Kurt"),
+    ("Hi Kurt!"),
     ('®')
 ]
 
-@pytest.mark.parametrize('l', PARAMS_TABLE)
-def test_echo(l):
-    from client import client
-    assert client(l) == l
+
 """messages shorter than one buffer in length"""
 """messages longer than several buffers in length"""
 """messages that are an exact multiple of one buffer in length"""
 """messages containing non-ascii characters"""
+
+
+@pytest.mark.parametrize('l', PARAMS_TABLE)
+def test_echo(l):
+    """Test confirms client receives messages it sent to server."""
+    from client import client
+    assert client(l) == l
