4b1739b5d563b4b1079b28286fc03296f63a19f9
Kurt <kurtrm@gmail.com>
2017-05-30 11:55:16 -0700

    Removed regex files and removed print statements.

--- src/server.py
+++ src/server.py
@@ -13,7 +13,6 @@ CRLF = '\r\n\r\n'
 def response_ok(uri):
     """Send an ok response."""
     response_ok = 'HTTP/1.1 200 OK{}{}{}'.format(LINE_BREAK, uri, CRLF)
-    print(response_ok)
     return response_ok.encode('utf-8')
 
 
@@ -23,20 +22,20 @@ def response_error(error):
         '405': 'HTTP/1.1 405 Method Not Allowed',
         '505': 'HTTP/1.1 505 HTTP Version Not Supported'
     }
-    print(error, error in error_dict)
-    response_error = '{}{}'.format(error_dict.get(error, '400 Not Found'), CRLF)  # default needs editing
-    print(error, 'response error before encoding', response_error)
+    response_error = '{}{}'.format(
+        error_dict.get(error, '400 Not Found'), CRLF)
     return response_error.encode('utf-8')
 
 
 def parse_request(client_request):
     """Parse client HTTP request and raise errors."""
-    get_present = re.compile(r'POST|PUT|HEAD|CONNECT|DELETE|OPTIONS|TRACE|PATCH')
-    version_correct = re.compile(r'HTTP/1.1')
+    get_present = re.compile(
+        r'POST|PUT|HEAD|CONNECT|DELETE|OPTIONS|TRACE|PATCH')
+    version_correct = re.compile(r'HTTP/[^1]\.1|HTTP/[^1]\.[^1]|HTTP/1\.[^1]')
     http_regex = re.compile(r'''(
         (GET\s)
         ([^\s]+\s)
-        (HTTP/1.1)
+        (HTTP/1\.1)
         (\r\n)
         (Host:\s)
         ([^\s]+)
@@ -44,16 +43,12 @@ def parse_request(client_request):
         )''', re.VERBOSE)
     mo = http_regex.match(client_request)
     if mo is None:
-        print('before if')
         if get_present.match(client_request) is not None:
-            print('in if')
             raise ValueError('405')
-        elif version_correct.match(client_request) is None:
-            print('in elif')
+        elif version_correct.match(client_request) is not None:
             raise ValueError('505', '404')
         raise ValueError()
     uri = '{}{}'.format(mo.group(2), mo.group(3))
-    print('uri', uri)
     return response_ok(uri)
 
 
@@ -65,6 +60,7 @@ def server():
     address = ('127.0.0.1', 5000)
     server.bind(address)
     server.listen(1)
+    print('Server running on port 5000')
 
     try:
         while True:
@@ -78,12 +74,9 @@ def server():
                 client_message += part
                 if client_message.decode('utf-8').endswith(CRLF):
                     complete = True
-
-            print('server received: ', client_message.decode('utf-8'))
             try:
                 conn.sendall(parse_request(client_message.decode('utf-8')))
             except ValueError as x:
-                print('except statement x:', x)
                 conn.sendall(response_error(x[0]))
             conn.close()
 
