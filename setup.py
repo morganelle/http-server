e2af7675cf05097092c908a270cb692505bfe2b1
Morgan Nomura <morganelle@gmail.com>
2017-07-23 23:55:37 -0700

    updated setup requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name="http-server",
-    description="A script that uses a client and server to echo a message.",
+    description="A concurrent web server.",
     version=0.1,
     author="Morgan, Ronel, Kurt",
     author_email="",
     license='MIT',
     py_modules=['client', 'server'],
     package_dir={'': 'src'},
+    install_requires=['gevent'],
     extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
     entry_points={
         'console_scripts': [
